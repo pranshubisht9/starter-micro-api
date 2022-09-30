bfd4cb49015a2b475ec01de11b7623b0d01a45ee
pranshubisht9 <pranshubisht9@gmail.com>
2022-09-30 14:00:34 +0530

    Establishing Server

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    console.log(`Just got a request at ${req.url}!`)
-    res.write('Yo!');
-    res.end();
-}).listen(process.env.PORT || 3000);
+const PORT = process.env.PORT || 3000;
+const path = require("path");
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router(path.join(__dirname, "db.json"));
+const middlewares = jsonServer.defaults();
+server.use(middlewares);
+server.use(jsonServer.bodyParser);
+server.use("/api", router);
+server.listen(PORT, () =>
+  console.log(`JSON Server is running on port ${PORT}`)
+);
